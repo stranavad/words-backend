af88f035380267c92a42fadffd7009ff82b6e143
= <=>
2022-03-17 23:26:20 +0100

    added unit edit route and func

--- views/units.js
+++ views/units.js
@@ -7,6 +7,7 @@ const pool = require("../app");
 router.get("/", getAllUnits);
 router.get("/detailed", getAllUnitsExtended);
 router.post("/", jsonParser, createUnit);
+router.put("/", jsonParser, updateUnit);
 router.get("/words", jsonParser, getWordsInUnit);
 router.delete("/", jsonParser, deleteUnit);
 
@@ -15,17 +16,20 @@ module.exports = router;
 function getAllUnits(_, reshttp) {
 	pool.getConnection((err, connection) => {
 		if (err) throw err;
-		connection.query(`select id,name from units order by id desc`, (err, units) => {
-			connection.release();
-			if (err) throw err;
-			reshttp.status(200);
-			reshttp.end(
-				JSON.stringify({
-					message: "units",
-					units,
-				})
-			);
-		});
+		connection.query(
+			`select id,name from units order by id desc`,
+			(err, units) => {
+				connection.release();
+				if (err) throw err;
+				reshttp.status(200);
+				reshttp.end(
+					JSON.stringify({
+						message: "units",
+						units,
+					})
+				);
+			}
+		);
 	});
 }
 
@@ -119,33 +123,80 @@ function deleteUnit(req, reshttp) {
 				if (err) throw err;
 				connection.release();
 				reshttp.status(200);
-				reshttp.end(JSON.stringify({
-					message: 'Vymazano adios'
-				}))
-			})
-		})
-	})
+				reshttp.end(
+					JSON.stringify({
+						message: "Vymazano adios",
+					})
+				);
+			});
+		});
+	});
 }
 
 function getAllUnitsExtended(_, reshttp) {
 	pool.getConnection((err, connection) => {
 		if (err) throw err;
-		connection.query(`select units.id as id, units.name as name, unitwords.id as wordId from units left join unitwords on units.id = unitwords.unit`, (err, res) => {
-			if (err) throw err;
-			connection.release();
-			let returnData = {};
-			res.forEach((item) => {
-				if (returnData[item.id]) {
-					returnData[item.id].wordCount += 1;
+		connection.query(
+			`select units.id as id, units.name as name, unitwords.id as wordId from units left join unitwords on units.id = unitwords.unit`,
+			(err, res) => {
+				if (err) throw err;
+				connection.release();
+				let returnData = {};
+				res.forEach((item) => {
+					if (returnData[item.id]) {
+						returnData[item.id].wordCount += 1;
+					} else {
+						returnData[item.id] = {
+							id: item.id,
+							name: item.name,
+							wordCount: item.wordId ? 1 : 0,
+						};
+					}
+				});
+				reshttp.status(200);
+				reshttp.end(
+					JSON.stringify({
+						message: "units detailed",
+						data: Object.values(returnData),
+					})
+				);
+			}
+		);
+	});
+}
+
+function updateUnit(req, reshttp) {
+	const { id, name } = req.body;
+	pool.getConnection((err, connection) => {
+		if (err) throw err;
+		connection.query(
+			`select id from units where id = ${id}`,
+			(err, res) => {
+				if (err) throw err;
+				if (res.length > 0) {
+					connection.query(
+						`update units set name = '${name}' where id = ${id}`,
+						(err) => {
+							if (err) throw err;
+							connection.release();
+							reshttp.status(200);
+							reshttp.end(
+								JSON.stringify({
+									message: "updated",
+								})
+							);
+						}
+					);
 				} else {
-					returnData[item.id] = {id: item.id, name: item.name, wordCount: item.wordId ? 1 : 0}
+					connection.release();
+					reshttp.status(200);
+					reshttp.end(
+						JSON.stringify({
+							message: "warning",
+						})
+					);
 				}
-			})
-			reshttp.status(200);
-			reshttp.end(JSON.stringify({
-				message: 'units detailed',
-				data: Object.values(returnData),
-			}))
-		})
-	})
+			}
+		);
+	});
 }
